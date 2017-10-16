a330102ae5dd3cc4e50d96c9e0898d1c0ce8e66b
Renan de Souza Polisciuc <renan.de.souza@outlook.com>
2017-10-16 10:31:29 -0200

    SubBytes, ShiftRows

--- aes.cpp
+++ aes.cpp
@@ -4,9 +4,47 @@
 #include "aes.h"
 
 using namespace std;
+void subBytes(unsigned char * state) {
+  for(int i = 0; i < 16; i++)
+    state[i] = S_BOX[state[i]];
+}
 
-void help() {
-  cout << "./aes_gpu -i <entrada> -c <chave> -o <saida>" << endl;
+void shiftRows(unsigned char * state) {
+  unsigned char tmp[16];
+  tmp[0] = state[0];
+  tmp[4] = state[4];
+  tmp[8] = state[8];
+  tmp[12] = state[12];
+
+  tmp[1] = state[5];
+  tmp[5] = state[9];
+  tmp[9] = state[13];
+  tmp[13] = state[1];
+
+  tmp[2] = state[10];
+  tmp[6] = state[14];
+  tmp[10] = state[2];
+  tmp[14] = state[6];
+
+  tmp[3] = state[15];
+  tmp[7] = state[3];
+  tmp[11] = state[7];
+  tmp[15] = state[11];
+  memcpy(state, tmp, 16);
+}
+
+void aes(unsigned char * in_bytes, unsigned char * chave) {
+  unsigned char state[16];
+  memcpy(state, in_bytes, 16);
+  subBytes(state);
+  shiftRows(state);
+  for(int i = 0; i < 16; i++) {
+    printf("[%X]", state[i]);
+  }
+  cout << endl;
+
+  for(int i = 0; i < (R_ROUNDS -1); i++) {
+  }
 }
 
 int main(int argc, char ** argv) {
@@ -15,6 +53,12 @@ int main(int argc, char ** argv) {
 
   FILE * fin; // Pointer para o arquivo
   unsigned char * fbytes = NULL; //Bytes do arquivo
+  unsigned char chave[16] = {
+    0x0, 0x1, 0x2, 0x3,
+    0x4, 0x5, 0x6, 0x7,
+    0x8, 0x9, 0xA, 0xB,
+    0xC, 0xD, 0xE, 0xF
+  };
 
   fin = fopen(argv[1], "r"); //Abre o arquivo como leitura
 
@@ -42,7 +86,12 @@ int main(int argc, char ** argv) {
   if (bytes_read < fsize)
     memset((fbytes + bytes_read), 0, (fsize - bytes_read));
 
+  for(int i = 0; i < fsize; i+= 16) {
+    //Processa os bytes de 16 em 16
+    aes(fbytes + i, chave);
+  }
 
+  //Libera a memória alocada
   delete [] fbytes;
   fclose(fin);
   return 0;
